61abb43a7bee8c8b461a5337e55c2af5b5afa766
lee in cheol <ic1218@office.skhu.ac.kr>
2024-01-04 09:59:31 +0900

    manage 페이지 수정

--- src/component/manage.tsx
+++ src/component/manage.tsx
@@ -1,7 +1,5 @@
 import '../manage.css';
-import calender from '../assets/calendar.png';
 import heart from '../assets/hearts.png';
-import smile from '../assets/smile-icon.png';
 import exercise from '../assets/exercise.png';
 import meal from '../assets/spoon-and-fork.png';
 import pill from '../assets/pill.png';
@@ -17,53 +15,40 @@ function Manage(){
             <form>
             <div id="manage-content">
                 <div id='manage-content1'>
-                    <img className="manage-img" src={calender}/>
-                    <p className="calender-p">From the day</p>
-                    <p className='calender-p'>it started</p>
-                    <p id="calender-day">D+</p>
-                    <p id='calender-day2'>32</p>
-                    
+                    <img className="manage-img" src={heart}/>
+                    <p className="manage-p">Didn't you</p>
+                    <p className='manage-p'>take drugs today?</p>
+                    <button id='stopDrug-btn1'>Yes</button>
+                    <button id='stopDrug-btn2'>No</button>
                 </div>
                 <div id='manage-content2'>
-                    <img className="manage-img" src={heart}/>
-                    <p className="calender-p">Give up</p>
-                    <p className='calender-p'>drug effect</p>
-                    <p id="heart-info">32</p>
-                    <p id='heart-per'>%</p>
-                </div>
-                <div id='manage-content3'>
-                    <img className="manage-img" src={smile}/>
-                    <p className="calender-p">Emotional</p>
-                    <p className='calender-p'>details</p> 
-                    <p id="smile-info">Good</p>                      
-                </div>
-            </div>
-            <div id="manage-b-content">
-                <div id='manage-b-content1'>
                     <img className="manage-img" src={exercise}/>
-                    <p className="calender-p">Exercise</p>
+                    <p className="manage-p">Exercise</p>
                     <button id='exercise-btn1'>Yes</button>
                     <button id='exercise-btn2'>No</button>
                     
                 </div>
-                <div id='manage-b-content2'>
+            </div>
+            <div id="manage-b-content">
+                
+                <div id='manage-b-content1'>
                     <img className="manage-img" src={meal}/>
-                    <p className="calender-p">Meal</p>
+                    <p className="manage-p">Meal</p>
                     <button id='meal-btn1'>breakfast</button>
                     <button id='meal-btn2'>lunch</button>
                     <button id='meal-btn3'>dinner</button>
                     
                 </div>
-                <div id='manage-b-content3'>
+                <div id='manage-b-content2'>
                     <img className="manage-img" src={pill}/>
-                    <p className="calender-p">Take</p>
-                    <p className='calender-p'>medicine</p> 
+                    <p className="manage-p">Take</p>
+                    <p className='manage-p'>medicine</p> 
                     <button id='tm-btn1'>morning</button>
                     <button id='tm-btn2'>lunch</button>
                     <button id='tm-btn3'>evening</button>                     
                 </div>
             </div>
-            <button type='submit' id="manage-btn">Save</button>
+            <button type='submit' id="manage-btn">Submit</button>
             </form>
             
 
