5b74049b44bca6750133b292b2dc0102d2d2aaff
lee in cheol <ic1218@office.skhu.ac.kr>
2024-01-04 11:22:53 +0900

    googleMap 띄우기

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,8 +1,23 @@
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+
 function Map(){
+    
+    const mapStyles = {        
+        height: "100vh",
+        width: "100%"};
+        const defaultCenter = {
+            lat: 37.487752, lng: 126.825173
+          }
     return(
-        <>
-        
-        </>
+        <LoadScript
+  googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}
+        />
+     </LoadScript>
     )
 }
 export default Map;
