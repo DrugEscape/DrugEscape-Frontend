726a9848c11273e2cde611441328f31f08251846
lee in cheol <ic1218@office.skhu.ac.kr>
2024-01-08 23:44:10 +0900

    구글 로그인 창 연결

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,8 +1,21 @@
 import '../App.css'
 import medicine from '../assets/medicine.png'
 import { Link} from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 function Header(){
+    console.log(window.location.origin);
+    const handleLogin = () => {
+        const clientId = import.meta.env.VITE_GOOGLE_OAUTH_API_KEY; // .env 파일에서 Google OAuth 클라이언트 ID를 불러옵니다.
+        const redirectUri = encodeURIComponent(window.location.origin); // 리다이렉트 URI는 현재 페이지의 원점(프로토콜 + 호스트 + 선택적 포트)입니다.
+        const scope = encodeURIComponent('profile email'); // Google 로그인에서 요청할 스코프입니다.
+        const responseType = 'token'; // 응답 유형입니다. 'token'은 액세스 토큰을 의미합니다.
+        const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+
+        window.location.href = url; // Google 로그인 페이지로 리다이렉트합니다.
+        
+    };
+
     return(
         <>
          <div id="header">
@@ -26,7 +39,7 @@ function Header(){
                     <dt>
                         <Link to="/donate">Donate</Link>
                     </dt>
-                    <button id="header-login1" >Login</button>
+                    <button id="header-login1" onClick={handleLogin} >Login</button>
                     <button id="header-mypage" >
                         <Link to="/report">My Page</Link>
                     </button>
@@ -39,4 +52,5 @@ function Header(){
         
     )
 }
+
 export default Header;
