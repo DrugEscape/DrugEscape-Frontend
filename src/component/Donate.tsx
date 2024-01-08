2cdda3024ba80a811173805888ec22054fe4cefd
lee in cheol <ic1218@office.skhu.ac.kr>
2024-01-08 11:39:52 +0900

    donate 페이지

--- src/component/Donate.tsx
+++ src/component/Donate.tsx
@@ -1,9 +1,53 @@
 
+import '../donate.css';
+import React, { useState } from 'react';
 
 function Donate(){
+    const [isVisible, setIsVisible] = useState(false);
+    const [placeHolder,setplaceHolder] = useState('20000');
+    const [inputValue, setInputValue] = useState('');
+    function handlesubmit(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        if (Number(inputValue)>Number(placeHolder)){
+            alert("포인트가 부족합니다.");
+            setInputValue('');
+        }else{
+            setIsVisible(!isVisible);
+            setplaceHolder(String(Number(placeHolder)-Number(inputValue)));
+            setInputValue('');
+        }
+    }
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setInputValue(event.target.value);
+    }
+
     return(
         <>
-
+        <div id='donate-complete' className={isVisible ? 'visible' : 'hidden'}>
+            <p id="donate-complete1">Thank you</p>
+            <p id="donate-complete2">for your donation!</p>
+            <p id="donate-complete3">Heartfelt thanks for your generous contribution!</p>
+        </div>
+        <div id="donate-title">
+            <p id="donate-donate">Donate</p>
+            <p id='donate-p'>Make a difference today!</p>
+            <p id='donate-p1'>Make a difference today!
+                Your contribution, no matter the size, can positively impact lives.</p>
+            <p id='donate-p2'>Join us in spreading kindness
+                 through your generous donation.</p>
+        </div>
+        <div id="donate-content">
+            <div id="donate-point">
+                <form id="donate-form" onSubmit={handlesubmit}>
+                    <input type="number" id="donate-input" placeholder={placeHolder} value={inputValue}
+                    onChange={handleChange}
+                    >
+                    </input>
+                    <p id="donate-points">Points</p>
+                    <button type='submit' id="donate-button">Donated</button>
+                </form>
+            </div>
+        </div>
         </>
     )
 }
