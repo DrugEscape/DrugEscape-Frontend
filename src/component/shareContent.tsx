cb510220cd8b18cbcba0163781086bc5ee770cdc
lee in cheol <ic1218@office.skhu.ac.kr>
2024-01-27 21:23:02 +0900

    댓글 스크롤바

--- src/component/shareContent.tsx
+++ src/component/shareContent.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import '../share.css';
-import { HTMLInputTypeAttribute, useState } from 'react';
 
 interface shareContentProps {
     comment: { [key: string]: string[]; };
