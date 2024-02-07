1830f531266622bbc14cf800d0fe0786066eb118
lee in cheol <ic1218@office.skhu.ac.kr>
2024-02-07 21:24:21 +0900

    로그인수정

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -15,9 +15,11 @@ function Header(){
     const code = urlParams.get('code');
     
     const handleLogin = () => {
-        const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=902025458863-clfbloilmkds2mfs5bj6lhjeg0rhh32c.apps.googleusercontent.com&redirect_uri=https://main--benevolent-selkie-320b7f.netlify.app/drugescape/callback&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email`;
+        const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=902025458863-clfbloilmkds2mfs5bj6lhjeg0rhh32c.apps.googleusercontent.com&redirect_uri=http://drugescape.duckdns.org/drugescape/callback&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email`;
         window.location.href = url  // Google 로그인 페이지로 리다이렉트합니다.
+
     };
+    
     const handleLogout = () => {
         fetch('http://drugescape.duckdns.org/drugescape/logout', {
           method: 'POST',
@@ -37,7 +39,8 @@ function Header(){
         if (code) { 
           
           fetch(`http://drugescape.duckdns.org/drugescape/callback?code=${code}`, { // URL에 code 쿼리 파라미터 추가
-            method: 'GET',
+          mode: 'no-cors',
+          method: 'GET',
             headers: {
               'Content-Type': 'application/json',
             },
