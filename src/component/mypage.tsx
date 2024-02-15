e07035f3da6ca10f69c2b81d83b7a69684b06865
lee in cheol <ic1218@office.skhu.ac.kr>
2024-02-15 11:50:27 +0900

    mypage

--- src/component/mypage.tsx
+++ src/component/mypage.tsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import '../mypage.css';
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 function mypage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [picture, setPicture] = useState('');
 
     useEffect(() => {
         const accessToken = localStorage.getItem('accessToken');
@@ -15,6 +19,9 @@ function mypage() {
             })
                 .then(response => {
                     console.log(response.data);
+                    setUsername(response.data.name);
+                    setEmail(response.data.email);
+                    setPicture(response.data.picture);
                 })
                 .catch(error => {
                     console.error('mypage failed:', error);
@@ -23,7 +30,10 @@ function mypage() {
     }, []);
   return (
     <div id='mypage'>
-      <h1>마이페이지</h1>
+      <h1>{username}</h1>
+      <img src ={picture} alt='profile'/>
+      <p>{email}</p>
+
     </div>
   );
 }
